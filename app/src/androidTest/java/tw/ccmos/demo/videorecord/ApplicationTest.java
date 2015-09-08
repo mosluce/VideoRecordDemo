bd8d6dbb2d89049073a2e6f9f6fd59f7d8dd12ba
默司 <mosluce@mosideMac-mini.local>
2015-09-08 22:32:25 +0800

    init

---
+++ app/src/androidTest/java/tw/ccmos/demo/videorecord/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tw.ccmos.demo.videorecord;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
